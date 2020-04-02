33e2ccf9d08cd19a89938bd83a35d22f3a50beeb
magvtv <magutujr@gmail.com>
2020-04-02 10:58:01 +0300

    Added some custom js

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,1 +1,29 @@
 alert ("Something interesting about your birthday");
+
+
+//AkanNames
+
+const daysOfWeek ={"Saturday", "Sunday", "Monday", "Tuseday", "Wednesday", "Thursday", "Friday"};
+daysOfWeek.length; //
+
+const femaleNames ={"Ama", "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua"}
+
+const maleNames ={"Kwame", "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi"};
+maleNames.length; //
+
+if (dayOfWeek==Saturday) {
+
+
+
+
+
+
+
+function getAkanNames () {
+   const day= parseInt(document.getElementbyId("day").value);
+   const month= parseInt(document.getElementById("month").value);
+   const year= parseInt(document.getElementById("year").value);
+
+}
+
+   const century=doCenturies(year)
